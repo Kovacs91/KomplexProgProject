7783a598173068537b9710ba3893e3c4fffc2563
Kovacs91 <deacdavid2002@gmail.com>
2021-05-25 16:32:29 +0200

    A Program alapjai meg vannak

---
+++ projekt/projekt/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace projekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Állat> lista;
+        public MainWindow()
+        {
+            lista = new List<Állat>();
+            InitializeComponent();
+        }
+
+        private void beolvas(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog be = new OpenFileDialog();
+            be.Filter="Szöveges Állomány (*.txt)|*.txt|Minden Állomány (*.*)|*.*";
+            be.ShowDialog();
+            lista.Clear();
+            StreamReader sr = new StreamReader(be.FileName);
+            while (!sr.EndOfStream)
+            {
+                lista.Add(new Állat(sr.ReadLine()));
+
+            }
+            sr.Close();
+            adatok.Items.Add("Név,Eszmei érték, Védetté tették, Faj");
+            foreach (var item in lista)
+            {
+                adatok.Items.Add(item.nev + " " + item.érték + " " + item.védetté_tették + " " + item.típus); 
+            }
+        }
+
+        private void kilép(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult valasz = MessageBox.Show("Biztos kilépsz?", "Üzenet", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (valasz==MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
---
+++ projekt/projekt/Állat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt
+{
+    class Állat
+    {
+        public string nev;
+        public int érték;
+        public int védetté_tették;
+        public string típus;
+        public Állat(string s)
+        {
+            string[] sor = s.Split(';');
+            nev = sor[0];
+            érték = int.Parse(sor[1]);
+            védetté_tették = int.Parse(sor[2]);
+            típus = sor[3];
+        }
+    }
+}
